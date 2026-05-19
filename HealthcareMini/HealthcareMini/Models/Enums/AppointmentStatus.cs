e30a8403a5975debc18750461757757c2a7a78f5
AhmedMohanad <ahmed.mohanad9988@gmail.com>
2026-05-19 09:58:13 -0700

    add person inter face and UserRole, appointmentStatus enums

---
+++ HealthcareMini/HealthcareMini/Models/Enums/AppointmentStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthcareMini.Models.Enums
+{
+    public enum AppointmentStatus
+    {
+
+        Scheduled = 0,  // patient has booked waiting for visit day 
+        CheckedIn = 1,  // patient arrived  
+        Completed = 2,  // patient had his visit and left the clinic
+        Canceled  = 3,  // appointment was called off before the visit
+        NoShow    = 4   // patient did not show up and did not cancel .
+
+    }
+}
---
+++ HealthcareMini/HealthcareMini/Models/IPerson.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthcareMini.Models
+{
+    public interface IPerson
+    {
+        // this is the main interface for all people in the system, including patients, doctors, and staff.
+        // it is made to give more flixibility in the future if we want to add more types of people.
+
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }  
+
+   
+        [Required, MaxLength(200)]
+        public string Name { get; set; }
+
+        [MaxLength(20)]
+        public string? PhoneNumber { get; set; }
+
+
+
+    }
+}
